77d6df247e74aae6c7dadba5dfa7a489c789f18a
dcnbr <decembersnb@gmail.com>
2023-04-22 12:09:13 -0400

    Improve(?) fitness computation
    So the old fitness was problematic because individuals could achieve really
    high fitness by matching exactly with background noise. This could be
    fine if every frequency in the input 'gets' at least one individual, but
    that is hard to guarantee. Instead, we really want to focus on the most
    prominent frequencies in the input.
    
    So, new fitness is the *amplitude of the nearest-neighbour frequency* in the
    input. Seems better, but introduces the problem that the amplitude of
    the individual has no effect on fitness; an individual matched perfectly to
    a strong frequency is 'fit' even if it is inaudible. Not idea, but
    unsure how to fix.
    
    I think ideally we do something like expectation of individuals treated
    as latent variables in a mixture model, but that would bump us up a
    complexity class...

--- fitness.py
+++ fitness.py
@@ -7,6 +7,8 @@ def fitness_random(individual, input_grid, config):
 def nn_pitch(individual, spectrum, config):
     pitch = individual[0]
     amplitude = individual[1]
-    freq_idx = round( (pitch*1024) / (11025) )
-    return 1 - abs(pitch - spectrum[freq_idx][0])*abs(amplitude - spectrum[freq_idx][1])
+    freq_idx = max(0, min(1024, (round( (pitch*1024) / (11025) ))))
+    #return - abs(pitch - spectrum[freq_idx][0])*abs(amplitude - spectrum[freq_idx][1])*spectrum[freq_idx][1]
+    return spectrum[freq_idx][1]
+
 
